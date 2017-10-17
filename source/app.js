0dc333bb5e8fe775b7ced3d6eb51777fe1b1c284
mac <adl@nonamelab.ru>
2017-10-17 17:45:05 +0300

    work in progress

--- source/app.js
+++ source/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const config = require('../config.json');
+const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
 const serve = require('koa-static');
@@ -10,6 +11,20 @@ const controller = require('./controller.js');
 const middleware = require('./middleware.js');
 const ReactDOMServer = require('react-dom/server');
 const logger = require('../libs/logger.js')('wallet-app');
+const {renderToStaticMarkup} = require('react-dom/server');
+
+const DATA = {
+	user: {
+		login: 'samuel_johnson',
+		name: 'Samuel Johnson'
+	}
+};
+
+function getView(viewId) {
+	const viewPath = path.resolve(__dirname, 'views', `${viewId}.server.js`);
+	delete require.cache[require.resolve(viewPath)];
+	return require(viewPath);
+}
 
 router.get('/cards/', controller.getCards);
 router.post('/cards/', controller.createCard);
@@ -27,6 +42,14 @@ router.post('/cards/:id/fill', controller.fill)
 
 app.use(middleware.common);
 app.use(router.routes());
+
+
+router.get('/', (ctx) => {
+	const indexView = getView('index');
+	const indexViewHtml = renderToStaticMarkup(indexView(DATA));
+
+	ctx.body = indexViewHtml;
+});
 
 app.use(serve('./public'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,106 @@
+/*
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-	entry: './source/client/index.js',
+	externals: nodeExternals(),
+	entry: './source/app.js',
+	//target: 'node',
+
 	module: {
-		rules: [
-			{
+		rules: [{
 				test: /\.js$/,
-				exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['es2015','react']
-                  }
+				loader: 'babel-loader'
 			},
 			{
 				test: /\.css$/,
-				loader: 'style-loader!css-loader'
+				loader: 'ignore-loader'
 			}
 		]
 	},
+	node: {
+		fs: 'empty'
+	},
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public')
-    },
-    devtool: 'source-map',
-	watch: true
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	resolve: {
+		modules: [
+			path.resolve(__dirname)
+		]
+	}
+	//libraryTarget: 'umd' // ???????????
 };
+*/
+const fs = require('fs');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function getExternals() {
+	return fs.readdirSync('node_modules')
+		.concat(['react-dom/server'])
+		.filter((mod) => mod !== '.bin')
+		.reduce((externals, mod) => {
+			externals[mod] = `commonjs ${mod}`;
+			return externals;
+		}, {});
+}
+
+module.exports = [
+	{
+		entry: {
+			index: './source/views/index.src.js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+				},
+				{
+					test: /\.css$/,
+					loader: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: 'css-loader'
+					})
+				}
+			]
+		},
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, 'public')
+		},
+		plugins: [
+			new ExtractTextPlugin('[name].css')
+		],
+		watch: true
+	},
+	{
+		entry: {
+			index: './source/views/index.server.src.js'
+		},
+		target: 'node',
+		externals: getExternals(),
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+				},
+				{
+					test: /\.css$/,
+					loader: 'ignore-loader'
+				}
+			]
+		},
+		output: {
+			filename: '[name].server.js',
+			path: path.resolve(__dirname, 'source/views'),
+			libraryTarget: 'umd'
+		},
+		watch: true
+	}
+];
